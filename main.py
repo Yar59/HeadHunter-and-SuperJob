60b2e7442d8838b91f7c512ca869c2fcebb521ca
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-05-26 19:46:16 +0300

    refactored functions

--- main.py
+++ main.py
@@ -5,34 +5,49 @@ from dotenv import dotenv_values
 from terminaltables import AsciiTable
 
 
-def get_vacancies_hh(languages):
+def get_vacancies_hh(language):
     url = "https://api.hh.ru/vacancies"
+    vacancies_pages = []
 
-    for language in languages:
-        languages[language]["average"] = 0
-        languages[language]["vacancies_processed"] = 0
-        for page in count(0):
-            payload = {
-                "text": f"Программист {language}",
-                "area": 1,
-                "page": page,
-                "per_page": 100
-            }
-            response = requests.get(url, params=payload)
-            response.raise_for_status()
-            vacancies = response.json()["items"]
-            total_vacancies = response.json()["found"]
-            for vacancy in vacancies:
-                salary = calculate_rub_salary_hh(vacancy)
-                if salary:
-                    languages[language]["average"] += salary
-                    languages[language]["vacancies_processed"] += 1
-            languages[language]["vacancies_found"] = total_vacancies
-            if page >= response.json()['pages'] - 1:
-                break
-        languages[language]["average"] = int(languages[language]["average"] / languages[language]["vacancies_processed"])
-        print(f"Получены данные по языку {language}")
-    return languages
+    for page in count(0):
+        payload = {
+            "text": f"Программист {language}",
+            "area": 1,
+            "page": page,
+            "per_page": 100
+        }
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        vacancies_pages.append(response)
+        if page >= response.json()['pages'] - 1:
+            break
+
+    print(f"Получены данные по языку {language} от сервиса HH")
+    return vacancies_pages
+
+
+def get_vacancies_sj(language, key):
+    url = "https://api.superjob.ru/2.0/vacancies/"
+    headers = {
+        "X-Api-App-Id": key,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    vacancies_pages = []
+    for page in count(0):
+        payload = {
+            "keyword": f'Программист {language}',
+            "town": 4,
+            "page": page,
+            "count": 100
+        }
+        response = requests.get(url, headers=headers, params=payload)
+        response.raise_for_status()
+        vacancies_pages.append(response)
+        if not response.json()['more']:
+            break
+
+    print(f"Получены данные по языку {language} от сервиса SJ")
+    return vacancies_pages
 
 
 def calculate_rub_salary_hh(vacancy):
@@ -63,40 +78,6 @@ def calculate_salary(salary_from, salary_to):
     return None
 
 
-def get_vacancies_sj(languages, key):
-    url = "https://api.superjob.ru/2.0/vacancies/"
-    headers = {
-        "X-Api-App-Id": key,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    for language in languages:
-        languages[language]["average"] = 0
-        languages[language]["vacancies_processed"] = 0
-        languages[language]["vacancies_found"] = 0
-        for page in count(0):
-            payload = {
-                "keyword": f'Программист {language}',
-                "town": 4,
-                "page": page,
-                "count": 100
-            }
-            response = requests.get(url, headers=headers, params=payload)
-            response.raise_for_status()
-            vacancies = response.json()["objects"]
-            total_vacancies = response.json()["total"]
-            for vacancy in vacancies:
-                salary = calculate_rub_salary_sj(vacancy)
-                if salary:
-                    languages[language]["average"] += salary
-                    languages[language]["vacancies_processed"] += 1
-                    languages[language]["vacancies_found"] += total_vacancies
-
-            if not response.json()['more']:
-                break
-        languages[language]["average"] = int(languages[language]["average"] / languages[language]["vacancies_processed"])
-    return languages
-
-
 def make_clever_print(statistics, title):
     table_data = [
         (
@@ -122,25 +103,71 @@ def make_clever_print(statistics, title):
     return table_instance.table
 
 
-if __name__ == "__main__":
-    top_languages = {
-        "JavaScript": {},
-        "Java": {},
-        "Python": {},
-        "Ruby": {},
-        "PHP": {},
+def process_vacancies_hh(vacancies_pages):
+    language_params = {
+        "average": 0,
+        "vacancies_processed": 0,
+        "vacancies_found": 0,
     }
+    for vacancies_page in vacancies_pages:
+        vacancies = vacancies_page.json()["items"]
+        total_vacancies = vacancies_page.json()["found"]
+        for vacancy in vacancies:
+            salary = calculate_rub_salary_hh(vacancy)
+            if salary:
+                language_params["average"] += salary
+                language_params["vacancies_processed"] += 1
+        language_params["vacancies_found"] = total_vacancies
+    language_params["average"] = int(language_params["average"] / language_params["vacancies_processed"])
+    return language_params
+
+
+def process_vacancies_sj(vacancies_pages):
+    language_params = {
+        "average": 0,
+        "vacancies_processed": 0,
+        "vacancies_found": 0,
+    }
+    for vacancies_page in vacancies_pages:
+        vacancies = vacancies_page.json()["objects"]
+        total_vacancies = vacancies_page.json()["total"]
+        for vacancy in vacancies:
+            salary = calculate_rub_salary_sj(vacancy)
+            if salary:
+                language_params["average"] += salary
+                language_params["vacancies_processed"] += 1
+                language_params["vacancies_found"] += total_vacancies
+    language_params["average"] = int(language_params["average"] / language_params["vacancies_processed"])
+    return language_params
+
+
+if __name__ == "__main__":
+    top_languages = [
+        "JavaScript",
+        "Java",
+        "Python",
+        "Ruby",
+        "PHP",
+    ]
     sj_key = dotenv_values(".env")["SJ_SECRET_KEY"]
     try:
+        top_languages_info_hh = {}
+        top_languages_info_sj = {}
+        for language in top_languages:
+            vacancies_pages_hh = get_vacancies_hh(language)
+            top_languages_info_hh[language] = process_vacancies_hh(vacancies_pages_hh)
+            vacancies_pages_sj = get_vacancies_sj(language, sj_key)
+            top_languages_info_sj[language] = process_vacancies_sj(vacancies_pages_sj)
+
         print(
             make_clever_print(
-                get_vacancies_hh(top_languages),
+                top_languages_info_hh,
                 "Вакансии HeadHunter в Москве"
             ),
             end="\n\n"
         )
         print(make_clever_print(
-            get_vacancies_sj(top_languages, sj_key),
+            top_languages_info_sj,
             "Вакансии SuperJob в Москве"
         ))
     except requests.exceptions.HTTPError as error:
